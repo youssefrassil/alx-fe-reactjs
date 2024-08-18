f1186a5e08b882d3774f896c9ff2a6ca051eb2ec
youssefrassil <youfer18@gmail.com>
2024-08-18 23:16:02 +0100

    ll

---
+++ my-company/alx-react-app-props/src/App.jsx
@@ -0,0 +1,52 @@
+// src/App.jsx
+import React, { useState } from 'react';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import Counter from './components/Counter';
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
+import ProfilePage from './ProfilePage';
+import { UserContext } from './UserContext';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <Header />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <WelcomeMessage />
+      <Counter />
+      <ProfilePage />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Footer />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
---
+++ my-company/alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,16 @@
+// src/UserDetails.jsx
+import React from 'react';
+import { useUserContext } from './UserContext';
+
+function UserDetails() {
+  const userData = useUserContext();
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
