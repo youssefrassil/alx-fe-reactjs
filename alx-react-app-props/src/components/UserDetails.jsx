2db25702dc85bb2e8967df7439ceb55323ccf42f
youssefrassil <youfer18@gmail.com>
2024-08-18 21:34:51 +0100

    pp

---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useUser } from './UserContext';
+
+function UserDetails() {
+  const userData = useUser(); // Access context value
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
