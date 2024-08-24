31b35bab8be33e9b832b19bd1bcb1a475c756521
youssefrassil <youfer18@gmail.com>
2024-08-24 20:18:53 +0100

    dd

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,6 +3,19 @@ import create from 'zustand';
 const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  
+  // Add the deleteRecipe action
+  deleteRecipe: (recipeId) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId)
+  })),
+  
+  // Add the updateRecipe action
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+  })),
+
   setRecipes: (recipes) => set({ recipes }),
 }));
 
