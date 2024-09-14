eb3ea6ee8b4c3a2ad6bdfc71f71b47969b6ee571
youssefrassil <youfer18@gmail.com>
2024-09-14 19:05:14 +0100

    add text

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,7 +8,7 @@ function UserProfile() {
         alt="User" 
         className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" 
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">John Doe</h1>
       <p className="text-sm md:text-base text-gray-600">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
