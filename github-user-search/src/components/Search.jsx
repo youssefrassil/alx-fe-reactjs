31044996742886792ab8db3d93a860de0b05c2b7
youssefrassil <youfer18@gmail.com>
2024-09-21 16:31:32 +0100

    kk

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -17,7 +17,7 @@ const Search = () => {
       const data = await fetchUserData(username);
       setUserData(data);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
